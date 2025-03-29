ebcf5f9ed171ec3e4b2d582a90e7b7d17284b146
Fatima Razzaq <84225992+fatimarazzaq@users.noreply.github.com>
2025-03-29 09:39:25 +0000

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/voice-studio/',  // Set this to the repository name
   plugins: [
     react(),
     mode === 'development' &&
